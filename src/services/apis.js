5404526d4a62324a5dc23f76e8847942661206b1
walkwithsanskar <walkwithsanskar1@gmail.com>
2023-06-19 09:31:13 +0530

    ci

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL || "https://watchmebackendapi.onrender.com/api/v1"
+const BASE_URL =  "https://watchmebackendapi.onrender.com/api/v1"
 
 
 // auth api
